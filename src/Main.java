7f30d02fa34087872f2a13f989ec5a5465bbda6b
pobanzkj <pobanzkj@mail.uc.edu>
2023-09-17 18:51:52 -0400

    YearlyMaintenanceCost

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double fallcost = 750;
+        double springcost = 750;
+        double summercost = 750;
+        double wintercost = 750;
+        double totalcost = fallcost + springcost + wintercost + summercost;
+            System.out.println("Here is your Spring cost $" + springcost);
+            System.out.println("Here is your Summer cost $" + summercost);
+            System.out.println("Here is your Fall cost $" + fallcost);
+            System.out.println("Here is your Winter cost $" + wintercost);
+            System.out.println("Here is your total yearly cost $" + totalcost);
+        }
+    }
